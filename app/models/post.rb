c40d489ef1c7184b20727c57d6f4e5ec93cfb4d3
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-02-03 23:52:26 +0100

    Resolve changes requested fro reviewer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id'
+
+  after_save :update_post_counter
 
   def most_recent_comments
     comments.limit(5).order(created_at: :desc)
