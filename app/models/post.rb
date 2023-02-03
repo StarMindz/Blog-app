47ebd9c6f3a3921255040c56bd5a9779adb009dd
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-02-03 16:31:21 +0100

    Create Model classes and add associations with additional functions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def most_recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_post_counter
+    user.increment!(:post_counter)
+  end
 end
